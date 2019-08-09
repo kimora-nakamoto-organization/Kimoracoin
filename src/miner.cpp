dfea2e201ce484166a482b124e8e487086ba62d6
Rishabh <39197153+rishabhworking@users.noreply.github.com>
2019-08-09 12:06:34 +0530

    Block Generation Issue in chain

--- src/miner.cpp
+++ src/miner.cpp
@@ -571,7 +571,8 @@ void static KimoraMiner(const CChainParams& chainparams)
                 // Busy-wait for the network to come online so we don't waste time mining
                 // on an obsolete chain. In regtest mode we expect to fly solo.
                 do {
-                    if ((g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) > 0) && !IsInitialBlockDownload()) {
+                    //if ((g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) > 0) && !IsInitialBlockDownload()) {
+                    if ((g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) > 0)) {
                         break;
                     }
 
